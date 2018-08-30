2995b21522802e50102bd6cbaf96deef11838eee
Sudi07 <2236272938@qq.com>
2018-08-30 16:25:01 +0800

    vue

---
+++ src/components/front.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="front-wrapper">
+        <my-list></my-list>
+    </div>
+</template>
+
+<script>
+import myList from './myList'
+export default {
+    components: {
+        myList 
+    }
+}
+</script>
+
